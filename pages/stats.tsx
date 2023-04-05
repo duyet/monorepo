752d89ed8ed94ec2bdfe0f43ab09c05a1cd14eaa
Duyet Le <duyet.cs@gmail.com>
2023-04-05 15:47:44 +0700

    chore: wakatime using svg instead of png

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -10,8 +10,8 @@ import CloudflareAnalytics, {
 type Props = CloudflareAnalyticsProps
 
 const urls = [
-  'https://wakatime.com/share/@8d67d3f3-1ae6-4b1e-a8a1-32c57b3e05f9/e3bcf43a-620f-416f-b8ac-1f74fa16e4e2.png',
-  'https://wakatime.com/share/@8d67d3f3-1ae6-4b1e-a8a1-32c57b3e05f9/5fd68fc4-d79a-432b-8ae0-8c2474b13de0.png',
+  'https://wakatime.com/share/@8d67d3f3-1ae6-4b1e-a8a1-32c57b3e05f9/bec141b5-a112-445b-8c79-c2a5f37e3380.svg',
+  'https://wakatime.com/share/@8d67d3f3-1ae6-4b1e-a8a1-32c57b3e05f9/fcfd3d44-340f-4a50-ba31-1c97b66a5a62.svg',
   'https://github-readme-stats.vercel.app/api?username=duyet&show_icons=true&theme=vue&hide_border=true&custom_title=@duyet',
 ]
 
