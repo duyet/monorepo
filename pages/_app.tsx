89cc0f63e7dbb4152aa67c73fab8f32de3d20d74
Duyet Le <duyet.cs@gmail.com>
2023-04-15 23:10:45 +0700

    chore(ui): update font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,52 @@
 import 'tailwindcss/tailwind.css'
 
 import Head from 'next/head'
-import Script from 'next/script'
 import type { AppProps } from 'next/app'
 import { Auth0Provider } from '@auth0/auth0-react'
+import { Inter as FontSans } from 'next/font/google'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Analytics from '../components/Analytics'
 
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  display: 'swap',
+})
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Auth0Provider
-      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
-      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
-    >
-      <Head>
-        <title>Tôi là Duyệt</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta
-          name='description'
-          content='Data Engineer @ Fossil. I blog about web development, machine learning, data engineering and more.'
-        />
-      </Head>
+    <>
+      <style jsx global>{`
+        :root {
+          --font-sans: ${fontSans.style.fontFamily};
+        }
+      `}</style>
 
-      <Header />
+      <Auth0Provider
+        clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+        domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+      >
+        <Head>
+          <title>Tôi là Duyệt</title>
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <meta
+            name='description'
+            content='Data Engineer @ Fossil. I blog about web development, machine learning, data engineering and more.'
+          />
+        </Head>
 
-      <main className='py-5'>
-        <Component {...pageProps} />
-      </main>
+        <Header />
 
-      <Footer />
+        <main className='py-5'>
+          <Component {...pageProps} />
+        </main>
 
-      <Analytics />
+        <Footer />
 
-    </Auth0Provider>
+        <Analytics />
+      </Auth0Provider>
+    </>
   )
 }
