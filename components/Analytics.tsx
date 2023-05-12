05792ca6e3c33e8ab04c0f877d5b28fcbb639251
Duyet Le <duyet.cs@gmail.com>
2023-05-12 10:36:05 +0700

    feat: add ms clarity

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -30,6 +30,15 @@ export default function AnalyticWrapper() {
           (window,document,"//pageview.duyet.net/pageview.js");
         `}
       </Script>
+      <Script id='clarity' strategy='afterInteractive'>
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) };
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "h2lw6wemnl");
+      `}
+      </Script>
     </>
   )
 }
