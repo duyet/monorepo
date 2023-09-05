de9ddc10bbd5a303b01b472e1f989a55496d3cf6
Duyet Le <me@duyet.net>
2023-09-05 15:50:19 +0700

    feat(cv): add cv app

---
+++ apps/cv/app/layout.tsx
@@ -0,0 +1,19 @@
+import { Head } from '@duyet/components';
+
+export const metadata = {
+  title: 'Duyet Resume',
+  description: '',
+};
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: LayoutProps) {
+  return (
+    <html lang="en" style={{ width: '100%', margin: 0, padding: 0 }}>
+      <Head />
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    </html>
+  );
+}
