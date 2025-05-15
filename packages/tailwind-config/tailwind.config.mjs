e6f027e6a7fc906bdf37223aeb72e07616fba413
Duyet Le <me@duyet.net>
2025-05-15 20:13:16 +0700

    chore(cv): fix the build
    - Upgraded @radix-ui/react-hover-card to version 1.1.13 in multiple package.json files.
    - Updated @types/react and @types/react-dom to version 19.1.4 and 19.1.5 respectively.
    - Refactored hover card components for improved structure and consistency.
    - Removed deprecated entries from yarn.lock and adjusted tailwind.config.mjs.

--- packages/tailwind-config/tailwind.config.mjs
+++ packages/tailwind-config/tailwind.config.mjs
@@ -99,7 +99,6 @@ export default {
   ],
   plugins: [
     require('tailwind-highlightjs'),
-    require('tailwindcss-animate'),
     ...require('./tremor.theme.js').plugins,
     addVariablesForColors,
   ],
