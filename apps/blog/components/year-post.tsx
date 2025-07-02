a5bdfac00a8a20b4a8e84fb78af2c3584f671c2a
Duyet Le <me@duyet.net>
2025-07-02 11:49:11 +0700

    fix(blog): datetime not display

--- apps/blog/components/year-post.tsx
+++ apps/blog/components/year-post.tsx
@@ -30,17 +30,17 @@ export function YearPost({ year, posts, className }: YearPostProps) {
       <div className="flex flex-col gap-3">
         {posts.map((post: Post) => (
           <article
-            className="flex flex-row items-center justify-between gap-2"
+            className="flex flex-row items-center gap-4"
             key={post.slug}
           >
-            <Link as={post.slug} className="text-md truncate" href="/[...slug]">
-              {post.title}
-              <IsNewPost date={post.date} />
-              <IsFeatured featured={post.featured} />
-            </Link>
+              <Link as={post.slug} className="text-md hover:underline" href="/[...slug]">
+                {post.title}
+                <IsNewPost date={post.date} />
+                <IsFeatured featured={post.featured} />
+              </Link>
             <hr className="shrink grow border-dotted border-slate-200 opacity-50" />
-            <time className="flex-0 flex-nowrap overflow-hidden text-nowrap text-gray-400">
-              {dateFormat(post.date, 'MMM do')}
+            <time className="flex-shrink-0 text-sm font-mono text-muted-foreground whitespace-nowrap">
+              {dateFormat(post.date, 'MMM dd')}
             </time>
           </article>
         ))}
