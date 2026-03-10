060dc1dd194bbf5656d93d3cee34ddb1155e7616
duyetbot <duyetbot@users.noreply.github.com>
2026-03-11 04:13:52 +0700

    fix(libs): exclude next-routes from main exports to fix Cloudflare Pages Functions
    next-routes imports from 'next/server' which pulls in ua-parser-js
    that uses __dirname (incompatible with Cloudflare edge runtime).
    
    Agents app only uses cn and generateId from @duyet/libs, but the
    entire package gets bundled including next-routes.
    
    Solution: Exclude from main index exports, allow direct import as
    @duyet/libs/next-routes for apps that need it (blog, insights, etc.)
    
    Co-Authored-By: duyet <me@duyet.net>
    Co-Authored-By: duyetbot <duyetbot@users.noreply.github.com>

--- packages/libs/index.ts
+++ packages/libs/index.ts
@@ -6,6 +6,8 @@ export * from "./fetcher";
 export * from "./getRelatedPosts";
 export * from "./getSlug";
 export * from "./markdownToHtml";
-export * from "./next-routes";
+// next-routes excluded: imports next/server which pulls in ua-parser-js
+// (uses __dirname, incompatible with Cloudflare Pages Functions)
+// Import directly as: import { createPingRoute } from "@duyet/libs/next-routes";
 export * from "./object-utils";
 export * from "./utils";
